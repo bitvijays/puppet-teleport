8921282201ffee233fa6d2b1b5e365b5a75af6ff
bitvijays <vk12122@my.bristol.ac.uk>
2021-03-10 19:52:13 +0000

    Added multi-arch package download

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,16 @@ class teleport::install {
 
   include ::archive
 
+  if $facts['architecture'] == 'aarch64' {
+    $arch = 'arm64'
+  }
+  elsif $facts['architecture'] == 'armv7l' {
+    $arch = 'armv'
+  }
+  elsif $facts['architecture'] == 'amd64' {
+    $arch = 'amd64'
+  }
+
   file { $teleport::extract_path:
     ensure => directory,
   } ->
@@ -12,7 +22,7 @@ class teleport::install {
     ensure       => present,
     extract      => true,
     extract_path => $teleport::extract_path,
-    source       => "https://get.gravitational.com/teleport-v${teleport::version}-linux-amd64-bin.tar.gz",
+    source       => "https://get.gravitational.com/teleport-v${teleport::version}-linux-${arch}-bin.tar.gz",
     creates      => "${teleport::extract_path}/teleport"
   } ->
   file {
